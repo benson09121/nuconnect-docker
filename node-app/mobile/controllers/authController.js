9a5c0fa60718fc0af3f7e5cc58f9db0de5f5f56f
benson09121 <benson09.javier@gmail.com>
2025-05-23 02:08:11 +0800

    Refactor login function to remove user creation logic; only authenticate existing users.

--- node-app/mobile/controllers/authController.js
+++ node-app/mobile/controllers/authController.js
@@ -9,10 +9,6 @@ async function login(req, res) {
         if (result && result.length > 0) {
             const token = await userModel.generateToken(mail);
             res.json({ status: 200, message: "User Authenticated", token: token });
-        } else {
-            await userModel.createUser(id, mail, surname, givenName);
-            const token = await userModel.generateToken(mail);
-            res.json({ status: 200, message: 'User Created', token: token });
         }
     } catch (err) {
         res.status(500).json({ message: err.message });
